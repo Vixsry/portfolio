42b0eb3b08d250eee50a2148987e19f7ad820b2c
Vixsry <kingvikvik25@gmail.com>
2024-11-26 05:17:16 +0700

    update src/assets/components/main.js

--- src/assets/components/main.js
+++ src/assets/components/main.js
@@ -1,24 +1,47 @@
+// LOADING SCREEN JS
+function hideLoader() {
+document.getElementById('loaderWrapper').classList.add('hidden');
+document.getElementById('mainContent').style.display = 'block';
+}
+document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
+setTimeout(hideLoader, 1000);
+});
+setTimeout(hideLoader, 5000);
+});
+
+// NAVBAR SCRIPT JS
 document.addEventListener("DOMContentLoaded", function () {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('#navbarNav');
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('#navbarNav');
+if (navbarToggler && navbarCollapse) {
+document.addEventListener('click', function (event) {
+const isClickInsideNavbar = navbarCollapse.contains(event.target) || navbarToggler.contains(event.target);
+if (!isClickInsideNavbar && navbarCollapse.classList.contains('show')) {
+navbarCollapse.classList.remove('show');
+}
+});
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+link.addEventListener('click', function () {
+if (navbarCollapse.classList.contains('show')) {
+navbarCollapse.classList.remove('show');
+}
+});
+});
+} else {
+console.error("Navbar elements not found. Please check your HTML.");
+}
+});
 
-    if (navbarToggler && navbarCollapse) {
-        document.addEventListener('click', function (event) {
-            const isClickInsideNavbar = navbarCollapse.contains(event.target) || navbarToggler.contains(event.target);
-            if (!isClickInsideNavbar && navbarCollapse.classList.contains('show')) {
-                navbarCollapse.classList.remove('show');
-            }
-        });
-
-        const navLinks = document.querySelectorAll('.nav-link');
-        navLinks.forEach(link => {
-            link.addEventListener('click', function () {
-                if (navbarCollapse.classList.contains('show')) {
-                    navbarCollapse.classList.remove('show');
-                }
-            });
-        });
-    } else {
-        console.error("Navbar elements not found. Please check your HTML.");
-    }
+// SMOOTH SCROLL SCRIP
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function (e) {
+e.preventDefault();
+const targetElement = document.querySelector(this.getAttribute("href"));
+targetElement.scrollIntoView({
+behavior: "smooth",
+block: "start" 
+});
+});
 });
